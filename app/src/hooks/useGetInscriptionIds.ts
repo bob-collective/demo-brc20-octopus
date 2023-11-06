e367c33d42614870d2e104fe38b1a9ac19896f82
Tom Jeatt <hello@tomjeatt.com>
2023-11-06 14:58:58 +0000

    wip: unisat testnet endpoint

--- app/src/hooks/useGetInscriptionIds.ts
+++ app/src/hooks/useGetInscriptionIds.ts
@@ -1,20 +1,24 @@
 import { useQuery } from "@tanstack/react-query";
-import { getOrdinalsUrl } from "../utils/ordinals-service";
+// import { getOrdinalsUrl } from "../utils/ordinals-service";
 
 const useGetInscriptionIds = (bitcoinAddress: string | undefined) => {
-  const ordinalsUrl = getOrdinalsUrl();
-  console.log("ordinalsUrl", ordinalsUrl, "bitcoinAddress", bitcoinAddress);
+  // const ordinalsUrl = getOrdinalsUrl();
+  // console.log("ordinalsUrl", ordinalsUrl, "bitcoinAddress", bitcoinAddress);
 
   const ordinalIds = useQuery({
     queryKey: ["inscription-ids", bitcoinAddress],
     enabled: !!bitcoinAddress,
     queryFn: async () =>
-      await fetch(`${ordinalsUrl}/address/${bitcoinAddress}`, {
-        method: "GET",
-        headers: {
-          Accept: "application/json",
-        },
-      }).then((response) => response.json()),
+      await fetch(
+        `https://open-api-testnet.unisat.io/v1/indexer/address/${bitcoinAddress}/inscription-utxo-data`,
+        {
+          method: "GET",
+          headers: {
+            Authorization: `Bearer d4126c66d843ad55c884ecbf0412bedaaae2aef40ddd4d24b6f6731eed7d1fb7`,
+            Accept: "application/json",
+          },
+        }
+      ).then((response) => response.json()),
   });
 
   return ordinalIds.data;
