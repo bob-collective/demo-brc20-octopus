a288b812f20c850b385a772b42e08159d2ef70c4
Tom Jeatt <hello@tomjeatt.com>
2023-11-14 10:25:38 +0000

    fix snapId path

--- app/src/utils/btcsnap-utils.ts
+++ app/src/utils/btcsnap-utils.ts
@@ -198,7 +198,7 @@ declare global {
 
 const { ethereum } = window;
 
-const snapId = "local:http://localhost:8081";
+const snapId = "http://localhost:8081";
 
 export async function checkConnection(): Promise<boolean> {
   const snaps = await ethereum.request({
