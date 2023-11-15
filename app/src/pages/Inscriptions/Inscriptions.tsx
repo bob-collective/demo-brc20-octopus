b66ed266194af31ee09c44ebf73ec46053f95e29
Tom Jeatt <hello@tomjeatt.com>
2023-11-15 10:33:34 +0000

    wip: inscription table

--- app/src/pages/Inscriptions/Inscriptions.tsx
+++ app/src/pages/Inscriptions/Inscriptions.tsx
@@ -1,17 +1,33 @@
 import {
   CTA,
-  List,
-  ListItem,
+  Card,
+  Flex,
   Modal,
   ModalBody,
   ModalHeader,
+  Table,
 } from "@interlay/ui";
 import { StyledWrapper } from "./Inscriptions.style";
 import { useGetInscriptionIds } from "../../hooks/useGetInscriptionIds";
 import { H2 } from "@interlay/ui";
 import { useBtcSnap } from "../../hooks/useBtcSnap";
-import { useState } from "react";
+import { ReactNode, useMemo, useState } from "react";
 import Inscription from "../Inscription/Inscription";
+
+enum InscriptionsTableColumns {
+  INSCRIPTION = "inscription",
+  ACTIONS = "actions",
+}
+
+type InscriptionsTableRow = {
+  id: string;
+  [InscriptionsTableColumns.INSCRIPTION]: ReactNode;
+  [InscriptionsTableColumns.ACTIONS]: ReactNode;
+};
+
+// type InheritAttrs = Omit<TableProps, keyof Props | 'columns' | 'rows'>;
+
+// type InscrtipionsTableProps = Props InheritAttrs;
 
 const Inscriptions = (): JSX.Element => {
   const [isInscriptionOpen, setInscriptionOpen] = useState(false);
@@ -20,8 +36,6 @@ const Inscriptions = (): JSX.Element => {
   const { bitcoinAddress } = useBtcSnap();
   const inscriptionIds = useGetInscriptionIds(bitcoinAddress);
 
-  console.log("inscriptionIds", inscriptionIds);
-
   const handleShowInscription = (id: string) => {
     setInscriptionId(id);
     setInscriptionOpen(true);
@@ -29,19 +43,48 @@ const Inscriptions = (): JSX.Element => {
     console.log(inscriptionId);
   };
 
+  const columns = [
+    { name: "Asset", id: InscriptionsTableColumns.INSCRIPTION },
+    { name: "", id: InscriptionsTableColumns.ACTIONS },
+  ];
+
+  const rows: InscriptionsTableRow[] = useMemo(
+    () =>
+      inscriptionIds
+        ? inscriptionIds.map((id) => {
+            return {
+              id: id,
+              inscription: `${id}`,
+              actions: (
+                <Flex
+                  justifyContent="flex-end"
+                  gap="spacing4"
+                  alignItems="center"
+                >
+                  <CTA onPress={() => handleShowInscription(id)} size="small">
+                    Show
+                  </CTA>
+                  <CTA onPress={() => console.log("transfer")} size="small">
+                    Transfer
+                  </CTA>
+                </Flex>
+              ),
+            };
+          })
+        : [],
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    [inscriptionIds]
+  );
+
+  console.log("inscriptionIds", inscriptionIds);
+
   return (
     <>
       <StyledWrapper direction="column" gap="spacing4">
         <H2>Inscriptions</H2>
-        <List>
-          {inscriptionIds?.map((inscriptionId) => (
-            <ListItem key={inscriptionId}>
-              <CTA onPress={() => handleShowInscription(inscriptionId)}>
-                {inscriptionId}
-              </CTA>
-            </ListItem>
-          ))}
-        </List>
+        <Card>
+          <Table columns={columns} rows={rows} />
+        </Card>
       </StyledWrapper>
       <Modal
         isOpen={isInscriptionOpen}
