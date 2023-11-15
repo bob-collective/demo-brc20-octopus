9bdeb9c73df073fea70e4123c137ae655b229926
Tom Jeatt <hello@tomjeatt.com>
2023-11-15 08:02:15 +0000

    refactor: move CTA

--- app/src/components/Layout/Layout.tsx
+++ app/src/components/Layout/Layout.tsx
@@ -1,21 +1,11 @@
-import { CTA, Flex } from "@interlay/ui";
+import { Flex } from "@interlay/ui";
 import "react-modern-drawer/dist/index.css";
 import { Header } from "./Header";
 import { StyledMain } from "./Layout.styles";
 
-import { useBtcSnap } from "../../hooks/useBtcSnap";
-
 const Layout = ({ ...props }) => {
-  const { bitcoinAddress, connectBtcSnap } = useBtcSnap();
   return (
     <>
-      <CTA
-        size="small"
-        onPress={() => connectBtcSnap()}
-        disabled={!!bitcoinAddress}
-      >
-        {bitcoinAddress ? bitcoinAddress : "Connect Metamask"}
-      </CTA>
       <Flex direction="column">
         <Header />
         <StyledMain direction="column" {...props} />
