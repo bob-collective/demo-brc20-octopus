faa3a4a7c4ebf54998d350e3a60a5cba3b4a0266
Tom Jeatt <hello@tomjeatt.com>
2023-11-15 08:48:07 +0000

    wip: render inscription form in modal

--- app/src/components/Layout/Header.tsx
+++ app/src/components/Layout/Header.tsx
@@ -1,46 +1,67 @@
-import { CTA, Flex, Span } from "@interlay/ui";
+import {
+  CTA,
+  CTALink,
+  Flex,
+  Modal,
+  ModalBody,
+  ModalHeader,
+  Span,
+} from "@interlay/ui";
 import { Link } from "react-router-dom";
 import { StyledHeader } from "./Layout.styles";
 import { Logo } from "./Logo";
 import { useBtcSnap } from "../../hooks/useBtcSnap";
-import { shortAddress } from "../../utils/format";
+import { useState } from "react";
+import Inscribe from "../../pages/Inscribe/Inscribe";
+// import { shortAddress } from "../../utils/format";
 
 const Header = () => {
+  const [isOpen, setOpen] = useState(false);
+
   const { bitcoinAddress, connectBtcSnap } = useBtcSnap();
 
   return (
-    <StyledHeader
-      elementType="header"
-      alignItems="center"
-      justifyContent="space-between"
-    >
-      <Flex gap="spacing6">
-        <Logo />
-        <nav>
-          <Flex elementType="ul" gap="spacing5">
-            <li>
-              <Link to="/transfer">
-                <Span weight="bold">Transfer</Span>
-              </Link>
-            </li>
-            <li>
-              <Link to="/inscribe">
-                <Span weight="bold">Inscribe</Span>
-              </Link>
-            </li>
-          </Flex>
-        </nav>
-      </Flex>
-      <Flex>
-        <CTA
-          size="small"
-          onPress={() => connectBtcSnap()}
-          disabled={!!bitcoinAddress}
-        >
-          {bitcoinAddress ? shortAddress(bitcoinAddress) : "Connect Metamask"}
-        </CTA>
-      </Flex>
-    </StyledHeader>
+    <>
+      <StyledHeader
+        elementType="header"
+        alignItems="center"
+        justifyContent="space-between"
+      >
+        <Flex gap="spacing6">
+          <Logo />
+          <nav>
+            <Flex elementType="ul" gap="spacing5">
+              <li>
+                <Link to="/transfer">
+                  <Span weight="bold">Transfer</Span>
+                </Link>
+              </li>
+              <li>
+                <CTALink onPress={() => setOpen(true)}>
+                  <Span weight="bold">Inscribe</Span>
+                </CTALink>
+              </li>
+            </Flex>
+          </nav>
+        </Flex>
+        <Flex>
+          <CTA
+            size="small"
+            onPress={() => connectBtcSnap()}
+            disabled={!!bitcoinAddress}
+          >
+            {/* {bitcoinAddress ? shortAddress(bitcoinAddress) : "Connect Metamask"} */}
+            {bitcoinAddress ? bitcoinAddress : "Connect Metamask"}
+          </CTA>
+        </Flex>
+      </StyledHeader>
+      <Modal isOpen={isOpen} onClose={() => setOpen(false)}>
+        <ModalHeader>Inscribe</ModalHeader>
+        <ModalBody>
+          <Inscribe />
+        </ModalBody>
+      </Modal>
+    </>
   );
 };
 
