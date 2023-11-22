428b5bcaf868f3ce3834fa9a3a236ee7630e68c5
Tom Jeatt <hello@tomjeatt.com>
2023-11-22 11:11:38 +0000

    remove unnecessary routing

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,17 +1,12 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { Layout } from "./components";
 import Inscriptions from "./pages/Inscriptions/Inscriptions";
 import "./utils/yup.custom";
 
 function App() {
   return (
-    <BrowserRouter>
-      <Layout>
-        <Routes>
-          <Route element={<Inscriptions />} path="/" />
-        </Routes>
-      </Layout>
-    </BrowserRouter>
+    <Layout>
+      <Inscriptions />
+    </Layout>
   );
 }
 
