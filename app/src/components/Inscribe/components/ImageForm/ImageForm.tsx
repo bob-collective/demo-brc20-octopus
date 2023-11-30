f98eef68499fb0d97f322b9759f23472751927fb
Tom Jeatt <hello@tomjeatt.com>
2023-11-30 14:55:09 +0000

    remove console log

--- app/src/components/Inscribe/components/ImageForm/ImageForm.tsx
+++ app/src/components/Inscribe/components/ImageForm/ImageForm.tsx
@@ -37,10 +37,7 @@ const ImageForm = ({ onSuccess }: Props): JSX.Element => {
         "There was a problem inscribing your ordinal. Do you have enough BTC?"
       );
     },
-    onSuccess: () => {
-      console.log("on succe");
-      onSuccess();
-    },
+    onSuccess: () => onSuccess(),
   });
 
   const handleSubmit = async (values: ImageFormData) => {
